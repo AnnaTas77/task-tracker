d90de9e8fe7d08816266d8435653b27ce16c6b0b
Anna Tasheva <anna.tasheva90@gmail.com>
2024-04-22 16:42:09 +0100

    Added a testTodo to the DB in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const connectdb = require("./mongodb");
 const { getTodos, createTodo, deleteTodo, toggleTodoStatus } = require("./controllers/todoController");
+const Todo = require("./models/todoModel");
 
 const app = express();
 
@@ -10,12 +11,30 @@ const port = 5000;
 app.use(express.json());
 app.use(cors());
 
+// app.get("/", (req, res) => {
+//     res.send("HELLOOO!!!");
+// });
 
+app.get("/", getTodos);
 
 const start = async () => {
     try {
         await connectdb();
         app.listen(port, () => console.log(`Server is running on port ${port}.`));
+
+        // let testTodo = new Todo({
+        //     title: "Practise coding",
+        //     complete: true,
+        // });
+
+        // testTodo
+        //     .save()
+        //     .then((doc) => {
+        //         console.log("FROM DB: ", doc);
+        //     })
+        //     .catch((err) => {
+        //         console.error(err);
+        //     });
     } catch (error) {
         console.error(error);
         // process.exit(1);
